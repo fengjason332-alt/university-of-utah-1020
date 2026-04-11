65312b9ae740f399519c977960c4501b0714e33a
Jason Feng <fengjason332@gmail.com>
2026-04-10 18:15:45 -0600

    complete

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -1,124 +1,98 @@
-import math
+# GitHub repo: https://github.com/fengjason332-alt/university-of-utah-1020
 
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
+import sys
+import glob
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,
+                             QHBoxLayout, QSlider, QPushButton, QFrame, QMenuBar)
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt, QTimer
 
-# This function loads a series of sprite images stored in a folder with a
-# consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
-def load_sprite(sprite_folder_name, number_of_frames):
-    frames = []
-    padding = math.ceil(math.log(number_of_frames - 1, 10))
-    for frame in range(number_of_frames):
-        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
-        frames.append(QPixmap(folder_and_file_name))
 
-    return frames
-
-class SpritePreview(QMainWindow):
-
+class SpritePreviewer(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Sprite Animation Preview")
-        # This loads the provided sprite and would need to be changed for your own.
-        self.num_frames = 21
-        self.frames = load_sprite('spriteImages', self.num_frames)
+        self.setWindowTitle("Sprite Preview")
 
+        # Load all sprite images
+        self.sprites = sorted(glob.glob("spriteImages/sprite_*.png"))
         self.current_frame = 0
-        self.is_playing = False
 
+        # Main layout
+        main_layout = QVBoxLayout()
+
+        # Menu bar
+        menu_bar = QMenuBar()
+        file_menu = menu_bar.addMenu("File")
+        pause_action = file_menu.addAction("Pause")
+        pause_action.triggered.connect(self.pause)
+        exit_action = file_menu.addAction("Exit")
+        exit_action.triggered.connect(self.close)
+        main_layout.setMenuBar(menu_bar)
+
+        # Top area: sprite + slider side by side
+        top_layout = QHBoxLayout()
+
+        self.sprite_label = QLabel()
+        pixmap = QPixmap(self.sprites[0])
+        self.sprite_label.setPixmap(pixmap)
+        self.sprite_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        top_layout.addWidget(self.sprite_label)
+
+        self.slider = QSlider(Qt.Orientation.Vertical)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(100)
+        self.slider.setValue(1)
+        self.slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        self.slider.setTickInterval(20)
+        self.slider.valueChanged.connect(self.update_fps)
+        top_layout.addWidget(self.slider)
+
+        main_layout.addLayout(top_layout)
+
+        # FPS label
+        fps_layout = QHBoxLayout()
+        fps_layout.addWidget(QLabel("Frames per second"))
+        self.fps_value_label = QLabel("1")
+        fps_layout.addWidget(self.fps_value_label)
+        main_layout.addLayout(fps_layout)
+
+        # Start/Stop button
+        self.button = QPushButton("Start")
+        self.button.clicked.connect(self.toggle_animation)
+        main_layout.addWidget(self.button)
+
+        self.setLayout(main_layout)
+
+        # Timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.next_frame)
 
-        # Make the GUI in the setupUI method
-        self.setupUI()
-
-    def setupUI(self):
-        # An application needs a central widget - often a QFrame
-        frame = QFrame()
-        main_layout = QVBoxLayout()
-
-        # Sprite image display
-        self.image_label = QLabel()
-        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.image_label.setPixmap(self.frames[self.current_frame])
-        main_layout.addWidget(self.image_label)
-
-        # FPS label
-        fps_label = QLabel("Frames per second")
-        fps_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        main_layout.addWidget(fps_label)
-
-        # FPS slider (1–100, with tick marks)
-        self.fps_slider = QSlider(Qt.Orientation.Horizontal)
-        self.fps_slider.setMinimum(1)
-        self.fps_slider.setMaximum(100)
-        self.fps_slider.setValue(10)
-        self.fps_slider.setTickPosition(QSlider.TickPosition.TicksBelow)
-        self.fps_slider.setTickInterval(10)
-        self.fps_slider.valueChanged.connect(self.on_fps_changed)
-        main_layout.addWidget(self.fps_slider)
-
-        # Current FPS value display
-        self.fps_value_label = QLabel(str(self.fps_slider.value()))
-        self.fps_value_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        main_layout.addWidget(self.fps_value_label)
-
-        # Start/Stop button
-        self.start_stop_button = QPushButton("Start")
-        self.start_stop_button.clicked.connect(self.toggle_animation)
-        main_layout.addWidget(self.start_stop_button)
-
-        frame.setLayout(main_layout)
-        self.setCentralWidget(frame)
-
-        # File menu
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("File")
-
-        pause_action = QAction("Pause", self)
-        pause_action.triggered.connect(self.pause_animation)
-        file_menu.addAction(pause_action)
-
-        exit_action = QAction("Exit", self)
-        exit_action.triggered.connect(QApplication.instance().quit)
-        file_menu.addAction(exit_action)
-
-    def pause_animation(self):
-        if self.is_playing:
-            self.timer.stop()
-            self.is_playing = False
-            self.start_stop_button.setText("Start")
-
-    def on_fps_changed(self, value):
-        self.fps_value_label.setText(str(value))
-        if self.is_playing:
-            self.timer.setInterval(1000 // value)
-
-    def toggle_animation(self):
-        if self.is_playing:
-            self.timer.stop()
-            self.is_playing = False
-            self.start_stop_button.setText("Start")
-        else:
-            fps = self.fps_slider.value()
-            self.timer.start(1000 // fps)
-            self.is_playing = True
-            self.start_stop_button.setText("Stop")
+    def update_fps(self, fps):
+        self.fps_value_label.setText(str(fps))
+        if self.timer.isActive():
+            self.timer.setInterval(int(1000 / fps))
 
     def next_frame(self):
-        self.current_frame = (self.current_frame + 1) % self.num_frames
-        self.image_label.setPixmap(self.frames[self.current_frame])
+        self.current_frame = (self.current_frame + 1) % len(self.sprites)
+        pixmap = QPixmap(self.sprites[self.current_frame])
+        self.sprite_label.setPixmap(pixmap)
 
+    def toggle_animation(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.button.setText("Start")
+        else:
+            fps = self.slider.value()
+            self.timer.start(int(1000 / fps))
+            self.button.setText("Stop")
 
-def main():
-    app = QApplication([])
-    # Create our custom application
-    window = SpritePreview()
-    # And show it
-    window.show()
-    app.exec()
+    def pause(self):
+        self.timer.stop()
+        self.button.setText("Start")
 
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = SpritePreviewer()
+    window.show()
+    sys.exit(app.exec())
